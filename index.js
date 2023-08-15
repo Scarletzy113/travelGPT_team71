de7dae9068d8aacb7af01609a556c5e82a291195
Scarletzy113 <120085488+Scarletzy113@users.noreply.github.com>
2023-08-16 00:34:16 +0800

    Update index.js
    added recommendations

--- index.js
+++ index.js
@@ -242,6 +242,138 @@ app.post("/hotel/result", requireLogin, (req, res, next) => {
 });
 
 // ... (Other routes and app.listen)
+app.get("/recommendations/search",requireLogin, (req, res) => {
+  res.render('recommendations-search');
+});
+
+
+
+app.post("/recommendations/result", requireLogin, (req, res, next) => {
+  const userId = req.session.user.user_id;
+  const country = req.body.country;
+  const { format } = require('date-fns');
+  const pax = req.body.pax;
+  // Inside your POST route handler
+  const startDate = req.body.startDate; // Assuming format is dd-mm-yyyy
+  const endDate = req.body.endDate; // Assuming format is dd-mm-yyyy
+  
+  // Convert the dates to the required format (yyyy-mm-dd)
+  const formattedStartDate = format(new Date(startDate), 'yyyy-MM-dd');
+  const formattedEndDate = format(new Date(endDate), 'yyyy-MM-dd');
+
+  
+
+
+  // Corrected SQL syntax for updating user's country
+  const updateUserSql = "UPDATE users SET country = ? WHERE user_id = ?";
+  const userData = [country, userId];
+
+  db.run(updateUserSql, userData, function (err) {
+    if (err) {
+      next(err); // Send the error on to the error handler
+    } else {
+      console.log(`User's country updated for user ID ${userId}`);
+      console.log(`country saved ${country}`);
+      const options = {
+        method: 'GET',
+        url: 'https://travel-advisor.p.rapidapi.com/locations/v2/auto-complete',
+        params: {
+          query: country,
+          lang: 'en_US',
+          units: 'km'
+        },
+        headers: {
+          'content-type': 'application/json',
+          'X-RapidAPI-Key': 'b519162a41msh5b10209e96c5027p107049jsn63c119b6732e',
+          'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
+        },
+       
+      };
+
+      axios.request(options)
+        .then((response) => {
+          
+          const geoID = response.data.data.Typeahead_autocomplete.results[3].detailsV2.locationId;
+
+          // Corrected SQL syntax for updating user's geoID
+          const updateGeoIDSql = "UPDATE users SET geoId = ? WHERE user_id = ?";
+          const geoIDData = [geoID, userId];
+
+          db.run(updateGeoIDSql, geoIDData, function (err) {
+            if (err) {
+              next(err); // Send the error on to the error handler
+            } else {
+              console.log(`User's geoId updated for user ID ${userId} with geoId: ${geoID}`);
+
+              const options2 = {
+                method: 'POST',
+                url: 'https://travel-advisor.p.rapidapi.com/attractions/v2/list',
+                params: {
+                  currency: 'USD',
+                  units: 'km',
+                  lang: 'en_US'
+                },
+                headers: {
+                  'content-type': 'application/json',
+                  'X-RapidAPI-Key': 'b519162a41msh5b10209e96c5027p107049jsn63c119b6732e',
+                  'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
+                },
+                data: {
+                  geoId: geoID,
+                  startDate: `${formattedStartDate}`,
+                  endDate: `${formattedEndDate}`,
+                  pax: [
+                    {
+                      ageBand: 'ADULT',
+                      count: pax
+                    }
+                  ],
+                  sort: 'TRAVELER_FAVORITE_V2',
+                  sortOrder: 'asc',
+                  updateToken: ''
+                }
+              };
+              
+              axios.request(options2)
+              .then((response2) => {
+                // console.log(formattedStartDate);
+                // console.log(formattedEndDate);
+                const sections = response2.data.data.AppPresentation_queryAppListV2[0].sections;
+
+                const recommendations = [];
+                            
+                for (const sectionKey in sections) {
+                  const section = sections[sectionKey];
+                  
+                  // Check if the section has a listSingleCardContent object
+                  if (section.listSingleCardContent && typeof section.listSingleCardContent === 'object') {
+                    const recommendation = {
+                      name: section.listSingleCardContent.cardTitle.string,
+                      rating: section.listSingleCardContent.bubbleRating ? section.listSingleCardContent.bubbleRating.rating : null,
+                      price: section.listSingleCardContent.merchandisingText?.htmlString?.string || 0,
+                      image: section.listSingleCardContent.cardPhoto.sizes.urlTemplate.replace('{width}',300).replace('{height}',200),
+                      
+                      // Add more fields as needed
+                    };
+                    recommendations.push(recommendation);
+                  }
+                }
+
+                console.log(recommendations);
+                // Pass the hotels array to your EJS template
+                res.render('recommendations', { recommendations });
+                })
+             
+            }
+          });
+        })
+        .catch((error) => {
+          next(error); // Handle API request error
+        });
+    }
+  });
+
+});
 
 
 
